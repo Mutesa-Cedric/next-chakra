20b801c5f59ed668e315aac3b76670d1b4322f1e
Mutesa-Cedric <mutesacedric@gmail.com>
2023-01-27 12:07:29 +0200

    fix : setup

---
+++ src/pages/index.tsx
@@ -0,0 +1,33 @@
+import {
+  Link as ChakraLink,
+  Text,
+  Code,
+  List,
+  ListIcon,
+  ListItem,
+} from '@chakra-ui/react'
+import { CheckCircleIcon, LinkIcon } from '@chakra-ui/icons'
+
+import { Hero } from '../components/Hero'
+import { Container } from '../components/Container'
+import { Main } from '../components/Main'
+import { DarkModeSwitch } from '../components/DarkModeSwitch'
+import { CTA } from '../components/CTA'
+import { Footer } from '../components/Footer'
+
+const Index = () => (
+  <Container height="100vh">
+    <Hero />
+    <Main>
+      Hello world
+    </Main>
+
+    <DarkModeSwitch />
+    <Footer>
+      <Text>Next ❤️ Chakra</Text>
+    </Footer>
+    <CTA />
+  </Container>
+)
+
+export default Index
